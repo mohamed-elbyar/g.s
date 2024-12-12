f02822efe34d8ccef888df0d230d62460ecd6682
mohamed elbiar <elbiarmohamed30@gmail.com>
2024-12-12 03:51:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var num1=+prompt("enter frist number")
+var num2=+prompt("enter socond number");
+var sum=num1+num2;
+document.write('<h1> the sum of number is<h1>')
+document.write(sum)
+var sub=num1-num2
+document.write('<h1> the subistracion of number is<h1>')
+document.write(sub)
+var multi=num1*num2
+document.write('<h1> the multiblaction of number is<h1>')
+document.write(multi)
+var exponentaion=num1**num2
+document.write('<h1> the exponentaion of number is<h1>')
+document.write(exponentaion)
+var modulus=num1%num2
+document.write('<h1> the modulus of number is<h1>')
+document.write(modulus)
+var div=num1/num2
+document.write('<h1> the division of number is<h1>')
+document.write(div)
